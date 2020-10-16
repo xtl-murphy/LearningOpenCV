daf21682d9c7a7695feb484031fea09791bef6b0
Murphy <771305239@qq.com>
2020-10-16 15:30:22 +0800

    [2020.10.16] 停止跟踪

--- cmake-build-debug/CMakeFiles/OpenCV_Sample_Median.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OpenCV_Sample_Median.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/python_loader/cv2/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BINARIES_PATHS = [
-    'D:/Documents/git_work/LearningOpenCV/cmake-build-debug/bin'
-] + BINARIES_PATHS
